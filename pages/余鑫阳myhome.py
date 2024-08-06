c1f01c39b16f93d87b8c00c6b4c2781baf4870e6
hanliuliu110 <hanyanwei@codemao.cn>
2024-08-06 13:30:37 +0800

    Add files via upload

--- pages/余鑫阳myhome.py
+++ pages/余鑫阳myhome.py
@@ -1,5 +1,6 @@
 '''我的主页'''
 import streamlit as st
+from PIL import Image
 page = st.sidebar.radio('我的首页', ['我的兴趣推荐', '图片处理工具', '数据收集', '工具分享', '留言区',])
 
 def page_1():
@@ -8,7 +9,28 @@ def page_1():
 
 def page_2():
     '''图片处理工具'''
-    pass
+    st.write("这个可以简单地处理图片（滤镜效果）:face_with_hand_over_mouth:操作如下:arrow_down:")
+    st.write(":one:点击右侧“Browesfiles”按钮，上传图片；上传图片后点击上方选项查看")
+    st.write("注：作者把自己壁纸（图片示例）放在在同一文件目录下了，可以先试试:wink:")
+    st.write(":sunglasses:图片换色小程序:sunglasses:")
+    uploaded_file = st.file_uploader("上传图片", type=['png', 'jpeg', 'jpg'])
+
+    if uploaded_file:
+        # 获取图片文件的名称、类型和大小
+        file_name = uploaded_file.name
+        file_type = uploaded_file.type
+        file_size = uploaded_file.size
+        img = Image.open(uploaded_file)
+        tab1,tab2,tab3,tab4 = st.tabs(["原图","改色1","改色2","改色3"])
+        with tab1:
+            st.image(img)
+        with tab2:
+            st.image(img_change(img, 0, 2, 1))
+        with tab3:
+            st.image(img_change(img, 1, 2, 0))
+        with tab4:
+            st.image(img_change(img, 1, 0, 2)) 
+        
     
 def page_3():
     '''数据收集'''
@@ -16,11 +38,44 @@ def page_3():
 
 def page_4():
     '''工具分享'''
-    pass
+    #第一个
+    st.write(":one:")
+    st.write(":sunglasses:图片换色小程序:sunglasses:")
+    uploaded_file = st.file_uploader("上传图片", type=['png', 'jpeg', 'jpg'])
 
+    if uploaded_file:
+        # 获取图片文件的名称、类型和大小
+        file_name = uploaded_file.name
+        file_type = uploaded_file.type
+        file_size = uploaded_file.size
+        img = Image.open(uploaded_file)
+        tab1,tab2,tab3,tab4 = st.tabs(["原图","改色1","改色2","改色3"])
+        with tab1:
+            st.image(img)
+        with tab2:
+            st.image(img_change(img, 0, 2, 1))
+        with tab3:
+            st.image(img_change(img, 1, 2, 0))
+        with tab4:
+            st.image(img_change(img, 1, 0, 2))
+    
 def page_5():
     '''留言区'''
     pass
+
+
+def img_change(img, rc, gc, bc):
+    '''图片处理'''
+    width, height = img.size
+    img_array = img.load()
+    for x in range(width):
+        for y in range(height):
+            r = img_array[x, y][rc]
+            g = img_array[x, y][gc]
+            b = img_array[x, y][bc]
+            img_array[x, y] = (r, g, b)
+    return img
+
 
 if page == '我的兴趣推荐':
     page_1()
